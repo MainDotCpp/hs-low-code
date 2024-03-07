74abfba4d80743395eecb55f3976a642734df211
yang yang <1400002030@qq.com>
2024-03-07 15:45:02 +0800

    feat: 对接斗蓬

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ import { check } from '@/app/lib/cloak/cloak';
 export async function middleware(request: NextRequest, response: NextResponse) {
   // 正则匹配uuid
   const uuidReg =
-    /\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/;
+    /^\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/;
   if (uuidReg.test(request.nextUrl.pathname)) {
     return check(request);
   }
